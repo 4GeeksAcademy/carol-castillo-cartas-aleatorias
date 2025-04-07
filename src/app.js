74c25fd59143a472ccd23833d99081d893d9766d
Carolina Castillo Lucas <carolina_c.l@hotmail.com>
2025-04-07 12:19:11 +0000

    cartas aleatorias

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 import "bootstrap";
 import "./style.css";
 
+function getRandomCard() {  
+  const cardValue = ["A","2","3","4", "5", "6", "7", "8", "9","10", "J", "Q", "K"];
+  const suits = [
+    {symbol: "♦", color: "red"},
+    {symbol: "♥", color: "red"},
+    {symbol: "♠", color: "black"},
+    {symbol: "♣", color: "black"}
+  ];
+  const randomValue = cardValue[Math.floor(Math.random()*cardValue.length)];
+  const randomSuit = suits[Math.floor(Math.random()*suits.length)];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  document.getElementById("card-number").textContent = randomValue;
+  document.getElementById("top-icon").textContent = randomSuit.symbol;
+  document.getElementById("bottom-icon").textContent = randomSuit.symbol;
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  const color = randomSuit.color
+  document.getElementById("card-number").style.color = color;
+  document.getElementById("top-icon").style.color = color;
+  document.getElementById("bottom-icon").style.color = color;
+}
+
+window.getRandomCard = getRandomCard;
+
+window.onload = getRandomCard;
+
+
+  
+ 
